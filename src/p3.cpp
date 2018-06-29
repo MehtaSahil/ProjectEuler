8df1cda6b42df74a38c738f21e1a3635fec7d9d2
Sahil Mehta <sahilmehta17@gmail.com>
2018-06-28 20:11:46 -0700

    finished problem 3

--- src/p3.cpp
+++ src/p3.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+#include <math.h>
 #include <utils/prime_utils.h>
 
 int main(int argc, char** argv) {
-  // uint64_t limit = 600851475143;
-  // for (uint64_t i = limit; i > 0; i--) {
-  //   std::cout << i << std::endl;
-  //   if (isPrimeFactor(i, limit)) {
-  //     std::cout << i << std::endl;
-  //     break;
-  //   }
-  // }
+  int64_t limit = 600851475143;
+  int64_t sqrt_limit = (int64_t) sqrt(limit) + 1;
 
-  std::vector<uint64_t> primes = genPrimesUnder(600851475143);
-  std::cout << primes.size() << std::endl;
+  // Instead of maximizing over all, just pick the first when iterating backward
+  for (uint64_t i = sqrt_limit; i > 0; i--) {
+    // Even numbers can't be prime factors
+    if (i % 2 == 0) {
+      continue;
+    }
+
+    if (isPrimeFactor(i, limit)) {
+      std::cout << i << std::endl;
+      break;
+    }
+  }
 
   return 0;
 }
